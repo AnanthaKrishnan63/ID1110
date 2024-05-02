0b92ec5f6fffa4501708e6dcd01db9ca6491849d
AnanthaKrishnan63 <krishnanthulsiraman2005@gmail.com>
2024-05-02 08:51:21 +0530

    update

--- test.py
+++ test.py
@@ -14,6 +14,10 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/')
+def main():
+    return redirect('/selection')
+
 @app.route('/selection', methods = ['GET','POST'])
 def selection():
     return render_template('selection1.html')
